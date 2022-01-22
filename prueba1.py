a9253b495ce091f236b1b1df2b64e7495e926dac
Valentín <pavicichvalentin@gmail.com>
2022-01-22 07:44:30 -0300

    dsdsd

--- prueba1.py
+++ prueba1.py
@@ -1,7 +1,14 @@
 #prueba1 de Git en Python
+#! Cuidado con esto
+#? No se si funcionará
+#* Atención a esto
+#// lo que dije ya no importa
+
+
 
 print("""
-Una prueba de texto
+Una forma de texto
 en la que se puede saltar líneas
-más facilmente en Python
+más fácilmente en Python.
+Es una genialidad.
 """)
